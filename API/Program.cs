81f3aa25bca4226e81c69d2262429c2a5366f8e8
Mariam Gamal <Mariaamgamaal25@gmail.com>
2023-09-03 02:38:12 +0300

    end of sec 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,15 +14,11 @@ opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 }); 
 
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
